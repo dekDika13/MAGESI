a492caeaf3b6a09408670595ac258bd72ebb6cd0
Dika <kadekdikaramasuryadi@gmail.com>
2024-11-24 16:32:07 +0800

    connect_to_database

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"magesi/database"
+
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	database.InitDB()
+
+}
